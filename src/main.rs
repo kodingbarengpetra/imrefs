4961757ca2685b16fad9bc56157d5a88a364f689
Petra Barus <petra.barus@kuncie.com>
2022-12-09 20:56:31 +0700

    successfully fork.

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,15 @@ fn init(name: &String) {
 
     match unsafe{fork()} {
         Ok(ForkResult::Parent { child, .. }) => {
-            println!("Continuing execution in parent process, new child has pid: {}", child);
-            waitpid(child, None).unwrap();
+            println!("Filesystem {} successfully created at /tmp/file1234.tmp with PID {}", name, child);
+            //waitpid(child, None).unwrap();
         }
         Ok(ForkResult::Child) => {
             // Unsafe to use `println!` (or `unwrap`) here. See Safety.
             write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
+            loop {
+                // Do nothing
+            }
             unsafe { libc::_exit(0) };
         }
         Err(_) => println!("Fork failed"),
