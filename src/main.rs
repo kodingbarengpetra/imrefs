afdf14ee74c96273f63e30928fcd1497ec742cf8
Petra Barus <petra.barus@kuncie.com>
2022-12-09 20:51:38 +0700

    success creating child process.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write}};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -28,9 +30,19 @@ fn main() {
 }
 
 fn init(name: &String) {
-    let tmpfile = "/tmp/file1234.tmp";
-    let pid = 1234;
-    println!("Filesystem {} successfully created at {} with PID {}", name, tmpfile, pid);
+
+    match unsafe{fork()} {
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!("Continuing execution in parent process, new child has pid: {}", child);
+            waitpid(child, None).unwrap();
+        }
+        Ok(ForkResult::Child) => {
+            // Unsafe to use `println!` (or `unwrap`) here. See Safety.
+            write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
+            unsafe { libc::_exit(0) };
+        }
+        Err(_) => println!("Fork failed"),
+     }    
 }
 
 fn send(name: &String, message: &String) {
