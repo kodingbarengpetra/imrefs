05f10e93081e1caba069d725f9cf8252e6bcf52a
Petra Barus <petra.barus@kuncie.com>
2022-12-09 20:02:27 +0700

    finish parsing.

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-main: main.rs
-	rustc main.rs
+all: dist/imrefs
+
+dist/imrefs: main.rs
+	rustc -o dist/imrefs main.rs
 
 clean:
 	rm -f main
--- main.rs
+++ main.rs
@@ -1,3 +1,40 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Not enough arguments");
+        return;
+    }
+
+    let command = &args[1];
+    let name = &args[2];
+
+    if command == "init" {
+        init(name);
+    } else if command == "send" {
+        if args.len() < 4 {
+            println!("Not enough arguments");
+            return;
+        }
+        let message = &args[3];
+        send(name, message);
+    } else if command == "stop" {
+        stop(name);
+    } else {
+        println!("Unknown command");
+    }
+}
+
+fn init(name: &String) {
+    println!("init {}", name);
+}
+
+fn send(name: &String, message: &String) {
+    println!("send {} {}", name, message);
+}
+
+fn stop(name: &String) {
+    println!("stop {}", name);
 }
